6f72578249473f52419f660fc942c207ac15c917
eddast <eddasr15@ru.is>
2018-09-11 15:51:31 +0000

    progress

--- Class Assignments/ClassAssignment4/Manifesto.Models/InputModels/BookInputModel.cs
+++ Class Assignments/ClassAssignment4/Manifesto.Models/InputModels/BookInputModel.cs
@@ -7,8 +7,8 @@ namespace Manifesto.Models.InputModels
     // Intentionally missing model validation. Your job is to implement that :)
     public class BookInputModel
     {
-        [Required]
-        [MinLength(3)]
+        [Required(ErrorMessage = "Book Name is required")]
+        [MinLength(3, ErrorMessage = "Book Name is at least three letters")]
         public string Name { get; set; }
         [Required]
         public string Author { get; set; }
@@ -16,6 +16,7 @@ namespace Manifesto.Models.InputModels
         [Required]
         public string ImageUrl { get; set; }
         public string Isbn { get; set; }
+        [RegularExpression("Games|Software Development|Romance|Self-help|Uncategorized", ErrorMessage = "Invalid category")]
         public string Category { get; set; }
         public int Pages { get; set; }
     }
