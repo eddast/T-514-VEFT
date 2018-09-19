d1416df6930ba9f8b81779beea474f82da00c762
eddast <eddasr15@ru.is>
2018-09-19 20:25:04 +0000

    setup structure and references

---
+++ Large Assignments/Large Assignment 1 - Technical Radiation/TechnicalRadiation.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TechnicalRadiation.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ Large Assignments/Large Assignment 1 - Technical Radiation/TechnicalRadiation.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TechnicalRadiation.Services
+{
+    public class Class1
+    {
+    }
+}
