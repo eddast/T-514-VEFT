da4fc41e5ea0c6ba538fee096319990b238c2745
eddast <eddasr15@ru.is>
2018-09-19 23:30:40 +0000

    added author data

--- Large Assignments/Large Assignment 1 - Technical Radiation/TechnicalRadiation.Models/Extensions/HyperMediaExtensions.cs
+++ Large Assignments/Large Assignment 1 - Technical Radiation/TechnicalRadiation.Models/Extensions/HyperMediaExtensions.cs
@@ -3,6 +3,6 @@ using System.Dynamic;
 
 namespace TechnicalRadiation.Models.Extensions {
   public static class HyperMediaExtensions {
-    public static void AddReference<T> (this ExpandoObject item, string key, T value) => item[key] = value;
+    // public static void AddReference<T> (this ExpandoObject item, string key, T value) => item[key] = value;
   }
 }
--- Large Assignments/Large Assignment 1 - Technical Radiation/TechnicalRadiation.Repositories/Data/AuthorDataProvider.cs
+++ Large Assignments/Large Assignment 1 - Technical Radiation/TechnicalRadiation.Repositories/Data/AuthorDataProvider.cs
@@ -18,7 +18,47 @@ namespace TechnicalRadiation.Repositories.Data
                 CreatedDate = DateTime.Now,
                 ModifiedDate = DateTime.Now,
                 ModifiedBy = _adminName
-            }
+            },
+            new Author
+            {
+                Id = 2,
+                Name = "Johnny Bravo",
+                ProfileImgSource = "https://ih0.redbubble.net/image.286736359.2796/flat,550x550,075,f.jpg",
+                Bio = "Hey, Baby! Anybody ever tell you I have beautiful eyes? Enough about you, let's talk about me, Johnny Bravo. Now remember, I do my best work when I'm being worshiped as a god.",
+                CreatedDate = DateTime.Now,
+                ModifiedDate = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new Author
+            {
+                Id = 3,
+                Name = "Ed",
+                ProfileImgSource = "https://pre00.deviantart.net/20bb/th/pre/f/2017/260/d/5/ed__edd_n_eddy___ed__2_by_ali_srn-dbnp4ax.png",
+                Bio = "I smell like fresh cut flowers sputing over a babling brook with a hint of lemon",
+                CreatedDate = DateTime.Now,
+                ModifiedDate = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new Author
+            {
+                Id = 4,
+                Name = "Obi Wan Kenobi",
+                ProfileImgSource = "https://starwarsblog.starwars.com/wp-content/uploads/sites/6/2017/08/star-wars-episode-3-obi-wan-mustafar.jpg",
+                Bio = "I have the high ground.",
+                CreatedDate = DateTime.Now,
+                ModifiedDate = DateTime.Now,
+                ModifiedBy = _adminName
+            },
+            new Author
+            {
+                Id = 5,
+                Name = "Anakin Skywalker",
+                ProfileImgSource = "https://vignette.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png/revision/latest?cb=20130621175844",
+                Bio = "I don't like sand. It's coarse and rough and irritating and it gets everywhere. Not like here. Here everything is soft and smooth.",
+                CreatedDate = DateTime.Now,
+                ModifiedDate = DateTime.Now,
+                ModifiedBy = _adminName
+            },
         };
     }
 }
