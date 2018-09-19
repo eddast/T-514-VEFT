63685be533cda37d9068e26087c104df1b940220
eddast <eddasr15@ru.is>
2018-09-19 22:41:22 +0000

    swagger comments

--- Large Assignments/Large Assignment 1 - Technical Radiation/TechnicalRadiation.WebApi/Controllers/NewsItemController.cs
+++ Large Assignments/Large Assignment 1 - Technical Radiation/TechnicalRadiation.WebApi/Controllers/NewsItemController.cs
@@ -4,16 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TechnicalRadiation.Models.Exceptions;
 using TechnicalRadiation.Models.InputModels;
 
 namespace TechnicalRadiation.WebApi.Controllers {
-  [Route ("api")]
+
+  /// <summary>
+  /// Used to manipulate and get information about news items
+  /// </summary>
+  [Route ("api/newsitems")]
   [ApiController]
   [Authorize]
   public class NewsItemController : ControllerBase {
 
-    // GET api
+    /// <summary>
+    /// Gets all news items
+    /// </summary>
+    /// <returns>A list of news items</returns>
     [Produces ("application/json")]
+    [ProducesResponseType(200)]
     [HttpGet]
     [Route ("")]
     [AllowAnonymous]
@@ -22,33 +31,57 @@ namespace TechnicalRadiation.WebApi.Controllers {
       return null;
     }
 
-    // GET api/5
-    [Produces ("application/json")]
+    /// <summary>
+    /// Gets news item by id
+    /// </summary>
+    /// <param name="id">Id which is associated with a news item within the system</param>
+    /// <returns>A single news item if found</returns>
+    [Produces("application/json")]
+    [ProducesResponseType(200)]
+    [ProducesResponseType(404)]
     [HttpGet]
-    [Route ("{newsItemId}")]
+    [Route ("{id}", Name = "GetNewsItemById")]
     [AllowAnonymous]
     public IActionResult GetNewsItemById (int newsItemId) {
       // TODO
       return null;
     }
 
-    // POST api
+    /// <summary>
+    /// Creates a new news item within the system
+    /// </summary>
+    /// <param name="model">The news item input model</param>
+    /// <returns>A status code of 201 and a set Location header.</returns>
+    [ProducesResponseType(201)]
+    [ProducesResponseType(412)]
     [HttpPost]
     public void CreateNewsItem ([FromBody] NewsItemInputModel model) {
-      if (!ModelState.IsValid) { /* TODO */ }
+      if (!ModelState.IsValid) { throw new InputFormatException("News item was not properly formatted."); }
       // TODO  
     }
 
-    // PUT api/5
-    [HttpPut ("{newsItemId}")]
+    /// <summary>
+    /// Updates a news item within the system
+    /// </summary>
+    /// <param name="id">Id which is associated with a news item within the system</param>
+    /// <param name="model">The news item input model</param>
+    /// <returns>A status code of 200 and a set Location header.</returns>
+    [ProducesResponseType(201)]
+    [ProducesResponseType(412)]
+    [HttpPut ("{id}")]
     public void EditNewsItem (int newsItemId, [FromBody] NewsItemInputModel model) {
-      if (!ModelState.IsValid) { /* TODO */ }
+      if (!ModelState.IsValid) { throw new InputFormatException("News item was not properly formatted."); }
       // TODO 
 
     }
 
-    // DELETE api/5
-    [HttpDelete ("{newsItemId}")]
+    /// <summary>
+    /// Deletes a news item from the system
+    /// </summary>
+    /// <param name="id">Id which is associated with a news item within the system</param>
+    /// <returns>A status code of 204 no content.</returns>
+    [ProducesResponseType(204)]
+    [HttpDelete ("{id}")]
     public void DeleteNewsItem (int newsItemId) {
       // TODO
     }
